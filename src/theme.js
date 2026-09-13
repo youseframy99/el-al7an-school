f3f97e71df689cc39b4ea3fb353077fa70cad6d2
youseframy99 <youseframy998@gmail.com>
2026-09-14 00:43:11 +0300

    Update

--- src/theme.js
+++ src/theme.js
@@ -95,7 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .handle-left-side {
       position: absolute;
       top: 55%;
-      right: 30px; /* على الحافة اليمين للدرفة الشمال (في المنتصف) */
+      right: 100px; /* على الحافة اليمين للدرفة الشمال (في المنتصف) */
       width: 10px;
       height: 90px;
       background: #ffffff;
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .handle-right-side {
       position: absolute;
       top: 55%;
-      left: 30px; /* على الحافة الشمال للدرفة اليمين (في المنتصف) */
+      left: 100px; /* على الحافة الشمال للدرفة اليمين (في المنتصف) */
       width: 10px;
       height: 90px;
       background: #ffffff;
